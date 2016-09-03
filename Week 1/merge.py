580e00746a89fa259e47f57538b51f3b495cf6f7
Timothy Samson <t.samson@nvusd.org>
2016-09-03 14:28:34 -0700

    Made merge a function def

--- Week 1/merge.py
+++ Week 1/merge.py
@@ -1,22 +1,19 @@
 # takes two sorted lists of numbers and produces a list that combines them that is sorted. 
 # The merge part in merge sort
+def merge(one, two):
+	c = []
 
-one = raw_input("LIST ONE\n").split()
-two = raw_input("LIST TWO\n").split()
+	for s in range (0, len(one) + len(two)):	
+		if not (one and two):
+			if not one:
+				c.extend(two)
+			else:
+				c.append(one)
+			return c
+			break
 
-c = []
-
-for s in range (0, len(one) + len(two)):	
-	if not (one and two):
-		if not one:
-			c.extend(two)
+		if one[0] < two[0]:
+			c.append(one.pop(0))
 		else:
-			c.append(one)
-		break
+			c.append(two.pop(0))
 
-	if one[0] < two[0]:
-		c.append(one.pop(0))
-	else:
-		c.append(two.pop(0))
-
-print c
