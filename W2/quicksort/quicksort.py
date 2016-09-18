edd395d30010e4c6a06e33c1b1b793f9b8a51f32
Timothy Samson <t.samson@nvusd.org>
2016-09-18 16:22:33 -0700

    Added an in-place quicksort implementation

--- W2/quicksort/quicksort.py
+++ W2/quicksort/quicksort.py
@@ -1,29 +1,40 @@
 import sys
 
 def quicksort(array):
+
     if not array:
         return []
 
+    def swap(i, j):
+        array[i], array[j] = array[j], array[i]
+
+    leftLen = 0
+    rightLen = 0
     pivot = array[0]
 
-    left = []
-    right = []
-    for i in array[1 :]:
-        if i < pivot:
-            left.append(i)
+    for i in xrange(1, len(array)):
+        if array[i] < pivot:
+            if not rightLen:
+                leftLen += 1
+                continue
+
+            swap(i, leftLen + 1)
+            leftLen += 1
         else:
-            right.append(i)
+            rightLen += 1
 
-    left = quicksort(left)
-    right = quicksort(right)
+    swap(0, leftLen)
 
-    left.append(pivot)
-    return left + right
+    array[0 : leftLen] = quicksort(array[0 : leftLen])
+    array[leftLen + 1 : ] = quicksort(array[leftLen + 1 :])
+
+    return array
 
 def main():
     args = map(int, sys.argv[1:])
-    print quicksort(args)
+    quicksort(args)
 
+    print args
 
 if __name__ == "__main__":
     main()
